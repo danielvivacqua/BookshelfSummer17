e15050ba168bbdcc16b6d7731f025f83723e750c
danielvivacqua <drvivacqua@hotmail.com>
2017-07-24 14:17:26 -0400

    Added Book and Category models

---
+++ BookshelfSummer17/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookshelfSummer17.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
